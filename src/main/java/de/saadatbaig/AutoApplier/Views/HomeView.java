ea5f917c1c57699e235ea543fa980d943b11a78a
Saadat-MBP16 <saadatdev@googlemail.com>
2020-09-10 22:40:39 +0200

    Update View - add editing prompt --> allows editing of vars or replacements via double click on the respective field - add according eventlisteners to the lists, so they can invoke the editor. - add a refreshing method, just for the lols.
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

--- src/main/java/de/saadatbaig/AutoApplier/Views/HomeView.java
+++ src/main/java/de/saadatbaig/AutoApplier/Views/HomeView.java
@@ -2,14 +2,20 @@ package de.saadatbaig.AutoApplier.Views;
 
 import de.saadatbaig.AutoApplier.Controllers.HomeViewController;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
+import javafx.util.Pair;
+
 import java.io.File;
+import java.util.Optional;
 
 public class HomeView {
 
@@ -29,7 +35,19 @@ public class HomeView {
 
     public void lateInit() {
         lvKeys.setItems(_controller._kvManager._keys);
+        lvKeys.setOnMouseClicked(evt -> {
+            if (evt.getClickCount() == 2 && !lvKeys.getItems().isEmpty()) {
+                int sel_idx = lvKeys.getSelectionModel().getSelectedIndex();
+                if (sel_idx != -1) { editValuePrompt(true, sel_idx); }
+            }
+        });
         lvVals.setItems(_controller._kvManager._vals);
+        lvVals.setOnMouseClicked(evt -> {
+            if (evt.getClickCount() == 2 && !lvVals.getItems().isEmpty()) {
+                int sel_idx = lvVals.getSelectionModel().getSelectedIndex();
+                if (sel_idx != -1) { editValuePrompt(false, sel_idx); }
+            }
+        });
     }
 
 
@@ -85,6 +103,25 @@ public class HomeView {
         alert.setContentText("Would you like to keep them?");
         alert.showAndWait();
         return alert.getResult() == ButtonType.YES;
+    }
+
+    public void editValuePrompt(boolean isKeys, int idx) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Edit Field");
+        dialog.setContentText("Value:");
+        dialog.setHeaderText("Enter a new value for the selected field");
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(str -> {
+            _controller.editInList(isKeys, idx, str);
+        });
+    }
+
+    /**
+     * (Just in case) Force refresh lists.
+     */
+    public void refreshLists() {
+        lvKeys.refresh();
+        lvVals.refresh();
     }
 
 
