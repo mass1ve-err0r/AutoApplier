749f5c777602093b69bc38ba87a022c49388bba9
Saadat-MBP16 <saadatdev@googlemail.com>
2020-09-10 21:57:21 +0200

    Update View - add filepicker - fix negative index passing --> Controller double-checks if it's -1. - add prompt whether to keep vars or start fresh if new file is loaded
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

--- src/main/java/de/saadatbaig/AutoApplier/Views/HomeView.java
+++ src/main/java/de/saadatbaig/AutoApplier/Views/HomeView.java
@@ -34,8 +34,28 @@ public class HomeView {
 
 
     @FXML
+    public void openFilePickerDialog(ActionEvent evt) {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Select Template Word Document");
+        fc.getExtensionFilters().add(
+                new ExtensionFilter("Word Document (2007+)", "*.docx")
+        );
+        Stage stage = (Stage) rootView.getScene().getWindow();
+        File rv = fc.showOpenDialog(stage);
+        if (rv != null) {
+            tfFName.setText(rv.getName());
+            tfFPath.setText(rv.getAbsolutePath());
+            if (lvKeys.getItems().isEmpty()) {
+                _controller.initDocx(false, rv.getAbsolutePath());
+            } else {
+                boolean b = saveVariablesPrompt();
+                _controller.initDocx(b, rv.getAbsolutePath());
+            }
+        }
+    }
+
+    @FXML
     public void removeReplPair(ActionEvent evt) {
-        //TODO: Catch no-selection case.
         int k_idx = lvKeys.getSelectionModel().getSelectedIndex();
         int v_idx = lvVals.getSelectionModel().getSelectedIndex();
         int idx = (k_idx == -1) ? v_idx : k_idx;
@@ -57,6 +77,14 @@ public class HomeView {
     @FXML
     public void startProcedure(ActionEvent evt) {
         System.out.println("startProcedure");
+    }
+
+    public boolean saveVariablesPrompt() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "", ButtonType.YES, ButtonType.NO);
+        alert.setTitle("Replacements are already present");
+        alert.setContentText("Would you like to keep them?");
+        alert.showAndWait();
+        return alert.getResult() == ButtonType.YES;
     }
 
 
